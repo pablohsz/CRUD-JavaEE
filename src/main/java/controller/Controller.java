4024e935143eb9ab7c59fbbff53b51a845a30e5d
@pablohsz <pablosouza.dev@gmail.com>
2022-07-05 22:25:51 -0300

    preparando classes para inserção no BD

--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -7,30 +7,48 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import model.DAO;
+import model.JavaBeans;
 
-
-@WebServlet(urlPatterns = {"/Controller", "/main", "/insert"})
+@WebServlet(urlPatterns = { "/Controller", "/main", "/insert" })
 public class Controller extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	DAO dao = new DAO();
-    
-    public Controller() {
-        super();
-        
-    }
+	JavaBeans contato = new JavaBeans();
+	
+	public Controller() {
+		super();
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		String action = request.getServletPath();
 		System.out.println(action);
-		if(action.equals("/main")){
+		if (action.equals("/main")) {
 			contatos(request, response);
+		} else {
+			if (action.equals("/insert")) {
+				novoContato(request, response);
+			} else {
+				response.sendRedirect("index.html");
+			}
 		}
 	}
 
-	
-	//ListarContatos
-	protected void contatos(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	// ListarContatos
+	protected void contatos(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		response.sendRedirect("agenda.jsp");
-		
+
+	}
+
+	protected void novoContato(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		 
+		// setar variáveis JavaBeans
+		contato.setNome(request.getParameter("nome"));
+		contato.setFone(request.getParameter("fone"));
+		contato.setEmail(request.getParameter("email"));
+
 	}
 }
